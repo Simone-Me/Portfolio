8c404e0754969262b3a1a0f440f4e6f9432784d6
Simone Melotti <simone.melotti96@gmail.com>
2024-05-27 10:44:20 +0200

    scroll bar

--- index.js
+++ index.js
@@ -190,3 +190,38 @@ function openDetail(tab) {
       .scrollIntoView({ behavior: "smooth" });
   }
 }
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector(".page-content");
+const slider2 = document.querySelector(".page-content2");
+
+var sliders = [slider, slider2];
+for (let index = 0; index < sliders.length; index++) {
+  const slider = sliders[index];
+
+  const startDragging = (e) => {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  };
+
+  const stopDragging = (e) => {
+    mouseDown = false;
+  };
+
+  const move = (e) => {
+    e.preventDefault();
+    if (!mouseDown) {
+      return;
+    }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+  };
+
+  // Add the event listeners
+  slider.addEventListener("mousemove", move, false);
+  slider.addEventListener("mousedown", startDragging, false);
+  slider.addEventListener("mouseup", stopDragging, false);
+  slider.addEventListener("mouseleave", stopDragging, false);
+}
